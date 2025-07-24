d093a947d44c8a63b12128ebc36391cd5b93edf7
Daniel George <dageorge1111@gmail.com>
2025-07-24 00:11:03 -0400

    added some logic on storing total io points

--- src/psar.rs
+++ src/psar.rs
@@ -9,11 +9,16 @@ use cron::Schedule;
 use chrono::Utc;
 use std::str::FromStr;
 use std::thread;
+use std::collections::HashMap;
 
+//TODO we need to figure out how to determine flushed vs buffered writes to quantify write_bytes contribution to iowait
+//For now we will just ignore write operations but we need to figure out how to measure write operations
 pub struct Psar<'a> {
     writer: &'a mut dyn Write,
     pid: pid_t,
     schedule: Schedule,
+    process_io_dict: HashMap<u32, u64>,
+    total_io: u64
 }
 
 impl<'a> Psar<'a> {
@@ -25,6 +30,8 @@ impl<'a> Psar<'a> {
                     writer,
                     pid: getpid(),
                     schedule: Self::getSarInterval(),
+                    process_io_dict: HashMap::new(),
+                    total_io: 0
                 }
             }
         } else {
@@ -33,7 +40,8 @@ impl<'a> Psar<'a> {
     }
 
     //Maybe race condition with task closing as we are reading?
-    //Maybe safely get processes running and match them with directory names rather than just using directory names?
+    //Maybe safely get processes running and match them with directory 
+    // names rather than just using directory names?
     pub fn start(&mut self) {
         loop {
             let datetime = self.schedule.upcoming(Utc).next().unwrap();
@@ -45,10 +53,21 @@ impl<'a> Psar<'a> {
                 let entry = entry.unwrap();
                 let mut path = entry.path();
                 let dirname = entry.file_name();
-                if let Some(name) = dirname.to_str() {
-                    if name.chars().all(|c| c.is_ascii_digit()) {
+                if let Some(proc_pid) = dirname.to_str() {
+                    if proc_pid.chars().all(|c| c.is_ascii_digit()) {
                         path.push("io");
-                        self.scrapeProcIO(path);
+                        match proc_pid.parse::<u32>() {
+                            Ok(pid) => {
+                                // parsed successfully
+                                writeln!(self.writer, "Parsed PID: {}", pid).unwrap();
+                            }
+                            Err(e) => {
+                                // failed to parse
+                                writeln!(self.writer, "Failed to parse PID {}", proc_pid).unwrap();
+                            }
+                        }
+                        self.scrapeProcIO(path, proc_pid.parse::<u32>().unwrap());
+                        //writeln!(self.writer, "total io points: {}", self.process_io_dict.get(&proc_pid.parse::<u32>().unwrap()).expect("REASON").to_string()).unwrap();                    
                     }
                 }
             }
@@ -82,7 +101,8 @@ impl<'a> Psar<'a> {
     fn getSarInterval() -> Schedule {
         if let Ok(lines) = Self::read_lines("/etc/cron.d/sysstat") {
             for line in lines.map_while(Result::ok) {
-                let tokens: Vec<&str> = line.trim().split_whitespace().collect();
+                let tokens: Vec<&str> = line
+                    .trim().split_whitespace().collect();
                 
                 if tokens.len() < 2 {continue;}
 
@@ -107,11 +127,33 @@ impl<'a> Psar<'a> {
         Ok(io::BufReader::new(file).lines())
     }
 
-    fn scrapeProcIO<P>(&mut self, ioFile: P)
+    fn scrapeProcIO<P>(&mut self, ioFile: P, proc_pid: u32)
     where P: AsRef<Path>, {
         if let Ok(lines) = Self::read_lines(ioFile) {
             for line in lines.map_while(Result::ok) {
                 writeln!(self.writer, "{}", line).unwrap();
+                let tokens: Vec<&str> = line
+                    .trim().split_whitespace().collect();
+
+                if tokens.len() < 2 {continue;}
+
+                let additional_io = tokens[1].parse::<u64>().unwrap();
+
+                match tokens[0] {
+                    "read_bytes::" => {
+                        self.process_io_dict.entry(proc_pid)
+                            .and_modify(|v| *v += additional_io)
+                            .or_insert(additional_io);
+                    }
+                    "write_bytes:" => {
+                        self.process_io_dict.entry(proc_pid)
+                            .and_modify(|v| *v += additional_io)
+                            .or_insert(additional_io);
+                    }
+                    &_ => {
+                        continue;
+                    }
+                }
             }
         }
     }
